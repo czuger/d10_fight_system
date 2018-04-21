152b0d511a677f845240f546696646f1d41116ad
ced <zuger.cedric@gmail.com>
2018-04-21 18:42:34 +0200

    Improving tests.

--- tests/test_damages.rb
+++ tests/test_damages.rb
@@ -7,7 +7,18 @@ class TestDamages < Minitest::Test
     dh = DamagesHandler.new
 
     assert_equal 'L', dh.wound( 6, '1d6', 0 )
+    assert_equal 'L', dh.wound( 5, '1d6', 0 )
+    assert_equal 'L', dh.wound( 5, '1d6', 1 )
+    assert_equal 'L', dh.wound( 6, '1d6', 1 )
+    refute dh.wound( 17, '1d6', 0 )
 
+    Kernel.stubs( :rand ).returns( 12 )
+    assert_equal 'C', dh.wound( 15, '1d12', 0 )
+    assert_equal 'G', dh.wound( 16, '1d12', 0 )
+
+    Kernel.stubs( :rand ).returns( 20 )
+    assert_equal 'C', dh.wound( 15, '1d12', 0 )
+    assert_equal 'G', dh.wound( 16, '1d12', 0 )
   end
 
 end
