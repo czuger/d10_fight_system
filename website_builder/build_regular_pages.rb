a033eeb9edfffe31de6731d0f9fe2d2fe8b3d423
ced <zuger.cedric@gmail.com>
2019-07-27 17:19:59 +0200

    Table integrated.

--- website_builder/build_regular_pages.rb
+++ website_builder/build_regular_pages.rb
@@ -29,15 +29,25 @@ def build_table( data, keys, table_anchor, locale )
   lower_header = keys.each_with_index.map{ |k, i| data[k].keys.sort.reverse.map{ |sk| "<th class='text-center target_#{i}_1'>#{dice_number_to_text(sk)}</th>" } }.flatten.join( '' )
   header = "<tr>#{upper_header}</tr><tr>#{lower_header}</tr>"
 
+  body_data = {}
   body = ''
   [ :critical_success, :success, :failure, :critical_failure ].each_with_index do |status, index|
     body_line = "<td>#{I18n.t('results.'+status.to_s)}</td>" + keys.each_with_index.map{ |k, keys_index| data[k].keys.sort.reverse.map{ |sk| stat_cell( data, k, sk, status, index, keys_index ) } }.flatten.join( '' )
+
     body += "<tr>#{body_line}</tr>"
   end
 
   table = "<table class='table table-striped'><thead>#{header}</thead><tbody>#{body}</tbody></table>\n"
 
-  add_lines_after "website/#{locale}/index.html", "<!--#{table_anchor}-->", table
+  # add_lines_after "website/#{locale}/index.html", "<!--#{table_anchor}-->", table
+
+  File.open( 'website_builder/table.html', 'w' ) do |f|
+    f.puts table
+  end
+
+  File.open( 'middleman/data/table.yaml', 'w' ) do |f|
+    f.puts data.to_yaml
+  end
 end
 
 def fill_text( locale )
@@ -57,7 +67,7 @@ def add_json_data( data, locale )
 
   # add_lines_after "website/#{locale}/index.html", "<!--JSON_CHARTS_DATA-->", "<input id='json_data' type='hidden' value='#{json_data.to_json}'>"
 
-  File.open( 'middleman/source/data/charts.yml', 'w' ) do |f|
+  File.open( 'middleman/data/charts.yml', 'w' ) do |f|
     f.puts json_data.to_yaml
   end
 end
