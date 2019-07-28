a430c58f57aa7c1f12ea59641cfe7eac60d9b47a
ced <zuger.cedric@gmail.com>
2019-07-28 07:45:00 +0200

    New deployment system.

---
+++ middleman/deploy.rb
@@ -0,0 +1,5 @@
+timestamp = Time.now.strftime( '%Y%m%d%H%M%S' )
+
+`scp -r build/ nw:/var/www/d20_vs_2d10_website/#{timestamp}`
+
+`ssh nw 'ln -s /var/www/d20_vs_2d10_website/#{timestamp} /var/www/d20_vs_2d10_website/build'`
