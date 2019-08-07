f709f1b2ccd398581ccb199a6b396d20d33c1248
ced <zuger.cedric@gmail.com>
2019-08-07 20:10:38 +0200

    Charts drawing part is more dry.

--- middleman/source/javascripts/charts.js
+++ middleman/source/javascripts/charts.js
@@ -1,3 +1,62 @@
+draw_chart = function( chart_id, title, formatter, serie_d10, serie_d20, max ) {
+
+    Highcharts.chart( chart_id, {
+
+        title: {
+            text: title
+        },
+
+        yAxis: {
+            title: {
+                text: '% succès'
+            },
+            max: max
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+
+        tooltip: {
+            formatter: function() { return formatter( this ) }
+        },
+
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 1
+            }
+        },
+
+        series: [{
+            name: 'd20',
+            data: serie_d10
+        }, {
+            name: '2d10',
+            data: serie_d20
+        }],
+
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+
+    });
+}
+
 window.onload = function () {
 
     var data=document.getElementById("json_data").value;
@@ -5,123 +64,85 @@ window.onload = function () {
 
     console.log( data );
 
-    Highcharts.chart('chart_regular_success', {
+    console.log( data.regular.success.strict_superiority );
 
-        title: {
-            text: 'Comparaison des chances de réussites entre un d20 et 2d10'
-        },
-
-        yAxis: {
-            title: {
-                text: '% succès'
+    draw_chart( 'chart_regular_success', 'Comparaison des chances de réussites entre un d20 et 2d10',
+        function ( chart ) {
+        // console.log( chart )
+            return 'Pour une difficulté de <b>' + chart.x +
+            '</b> les chances de réussite sont de <b>' + chart.y + '%</b>' +
+            ' pour ' + chart.series.name
             },
-            max: 100
+        data.regular.success.strict_superiority.d10, data.regular.success.superior_or_equal.d20, 100
+    )
+
+    draw_chart( 'chart_regular_success_equality', 'Comparaison des chances de réussites entre un d20 et 2d10 - equal',
+        function ( chart ) {
+            // console.log( chart )
+            return 'Pour une difficulté de <b>' + chart.x +
+                '</b> les chances de réussite sont de <b>' + chart.y + '%</b>' +
+                ' pour ' + chart.series.name
         },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
+        data.regular.success.superior_or_equal.d10, data.regular.success.superior_or_equal.d20, 100
+    )
 
-        tooltip: {
-            formatter: function () {
-                return 'Pour une difficulté de <b>' + this.x +
-                    '</b> les chances de réussite sont de <b>' + this.y + '%</b>' +
-                    ' pour ' + this.series.name;
-            }
-        },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 1
-            }
-        },
-
-        series: [{
-            name: 'd20',
-            data: data.regular.strict_superiority.d20
-        }, {
-            name: '2d10',
-            data: data.d10
-        }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-
-    });
-
-    Highcharts.chart('chart_critical_success', {
-
-        title: {
-            text: 'Comparaison des chances de réussites critiques entre un d20 et 2d10'
-        },
-
-        yAxis: {
-            title: {
-                text: '% succès'
-            },
-            max: 10
-        },
-        legend: {
-            layout: 'vertical',
-            align: 'right',
-            verticalAlign: 'middle'
-        },
-
-        tooltip: {
-            formatter: function () {
-                return 'Pour une difficulté de <b>' + this.x +
-                    '</b> les chances de réussite critique sont de <b>' + this.y + '%</b>' +
-                    ' pour ' + this.series.name;
-            }
-        },
-
-        plotOptions: {
-            series: {
-                label: {
-                    connectorAllowed: false
-                },
-                pointStart: 1
-            }
-        },
-
-        series: [{
-            name: 'd20',
-            data: data.d20_critical
-        }, {
-            name: '2d10',
-            data: data.d10_critical
-        }],
-
-        responsive: {
-            rules: [{
-                condition: {
-                    maxWidth: 500
-                },
-                chartOptions: {
-                    legend: {
-                        layout: 'horizontal',
-                        align: 'center',
-                        verticalAlign: 'bottom'
-                    }
-                }
-            }]
-        }
-
-    });
+    // Highcharts.chart('chart_critical_success', {
+    //
+    //     title: {
+    //         text: 'Comparaison des chances de réussites critiques entre un d20 et 2d10'
+    //     },
+    //
+    //     yAxis: {
+    //         title: {
+    //             text: '% succès'
+    //         },
+    //         max: 10
+    //     },
+    //     legend: {
+    //         layout: 'vertical',
+    //         align: 'right',
+    //         verticalAlign: 'middle'
+    //     },
+    //
+    //     tooltip: {
+    //         formatter: function () {
+    //             return 'Pour une difficulté de <b>' + this.x +
+    //                 '</b> les chances de réussite critique sont de <b>' + this.y + '%</b>' +
+    //                 ' pour ' + this.series.name;
+    //         }
+    //     },
+    //
+    //     plotOptions: {
+    //         series: {
+    //             label: {
+    //                 connectorAllowed: false
+    //             },
+    //             pointStart: 1
+    //         }
+    //     },
+    //
+    //     series: [{
+    //         name: 'd20',
+    //         data: data.regular.critical_success.strict_superiority.d20
+    //     }, {
+    //         name: '2d10',
+    //         data: data.regular.critical_success.strict_superiority.d10
+    //     }],
+    //
+    //     responsive: {
+    //         rules: [{
+    //             condition: {
+    //                 maxWidth: 500
+    //             },
+    //             chartOptions: {
+    //                 legend: {
+    //                     layout: 'horizontal',
+    //                     align: 'center',
+    //                     verticalAlign: 'bottom'
+    //                 }
+    //             }
+    //         }]
+    //     }
+    //
+    // });
 }
